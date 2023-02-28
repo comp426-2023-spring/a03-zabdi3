bae55a7a43ff7598509677b5fc517b58068793d9
Zackari Abdikarim <abdikarimz919@gmail.com>
2023-02-28 18:25:10 -0500

    help,rules,game function

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,1 +1,50 @@
-#!/usr/bin/node
+#!/usr/bin/env node
+import minimist from 'minimist';
+import { rps } from "../lib/rpsls.js"
+
+var args = minimist(process.argv.slice(2));
+
+if (args.h || args.help) {
+    help()
+}
+if (args.r || args.rules) {
+    rules()
+}
+// If an unlabeled argument is supplied but is out of range,
+// and the function should return an out-of-range error, 
+// and the error handler in the CLI wrapper should return a help/usage message AND listing of rules
+
+try {
+    console.log(JSON.stringify(rps(args._[0])));
+} catch (shoterror) {
+    if (shoterror instanceof RangeError) {
+        console.log(`Error: ${args._[0]} is out of range.`);
+        rules();
+    }
+}
+
+function help() {
+    console.log(
+        `Usage: node-rps [SHOT]
+        Play Rock Paper Scissors (RPS)
+        
+          -h, --help      display this help message and exit
+          -r, --rules     display the rules and exit
+        
+        Examples:
+          node-rps        Return JSON with single player RPS result.
+                          e.g. {"player":"rock"}
+          node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                          e.g {"player":"rock","opponent":"scissors","result":"win"}`
+    )
+}
+
+function rules() {
+    console.log(
+        `Rules for Rock Paper Scissors:
+
+        - Scissors CUTS Paper
+        - Paper COVERS Rock
+        - Rock CRUSHES Scissors`
+    )
+}
---
+++ bin/rpsls-cli.js
@@ -0,0 +1,62 @@
+#!/usr/bin/node
+import { rpsls } from "/lib/rpsls.js"
+import minimist from "minimist"
+
+
+//const args = minimist(process.argv.slice(2));
+var args = minimist(process.argv.slice(2));
+
+if (args.h || args.help) {
+    help()
+}
+if (args.r || args.rules) {
+    rules()
+}
+
+// If an unlabeled argument is supplied but is out of range,
+// and the function should return an out-of-range error, 
+// and the error handler in the CLI wrapper should return a help/usage message AND listing of rules
+
+try {
+    console.log(JSON.stringify(rpsls(args._[0])));
+} catch (shoterror) {
+    if (shoterror instanceof RangeError) {
+        console.log(`Error: ${args._[0]} is out of range.`);
+        rules();
+    }
+}
+
+function help() {
+    console.log(
+        `Usage: node-rpsls [SHOT]
+        Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+        
+          -h, --help        display this help message and exit
+          -r, --rules       display the rules and exit
+        
+        Examples:
+          node-rpsls        Return JSON with single player RPSLS result.
+                            e.g. {"player":"rock"}
+          node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                            e.g {"player":"rock","opponent":"Spock","result":"lose"}`
+    )
+    process.exit(0);
+}
+
+function rules() {
+    console.log(
+        `Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
+
+        - Scissors CUTS Paper
+        - Paper COVERS Rock
+        - Rock SMOOSHES Lizard
+        - Lizard POISONS Spock
+        - Spock SMASHES Scissors
+        - Scissors DECAPITATES Lizard
+        - Lizard EATS Paper
+        - Paper DISPROVES Spock
+        - Spock VAPORIZES Rock
+        - Rock CRUSHES Scissors`
+    )
+    process.exit(0);
+}
